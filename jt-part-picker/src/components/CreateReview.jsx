2ae5f103022a1e816b5692eabef3701f27bdc389
Joshua Thomas <jthoma10@hotmail.com>
2023-12-08 22:02:35 -0500

    Updates

--- jt-part-picker/src/components/CreateReview.jsx
+++ jt-part-picker/src/components/CreateReview.jsx
@@ -1,14 +1,55 @@
+import { useState } from "react"
+import axios from "axios"
+
+
+
+
 export default function CreateReview() {
+
+    const [title, setTitle] = useState('')
+    const [part, setPart] = useState('')
+    const [desc, setDesc] = useState('')
+
+    function handleTitleChange(event) {
+        setTitle(event.target.value)
+    }
+
+    function handlePartChange(event) {
+        setPart(event.target.value)
+    }
+
+    function handleDescChange(event) {
+        setDesc(event.target.value)
+    }
+
+
+    function handleSubmit(event)  {
+        const addReview = async() => {
+            const response = await axios.post(`http://localhost:3001/review`, {
+                title: title,
+                part: part,
+                description: desc
+            })
+            console.log(response.data)
+        }
+        addReview()
+    }
+
+
+
     return(
         <div className="addReview">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Create Review</h1>
                 <label>
-                    <input type="text" placeholder='Title' />
+                    <input type="text" placeholder='Title' value={title} onChange={handleTitleChange} />
+                </label>
+                <label>
+                    <input type="text" placeholder="Part" value={part} onChange={setPart} />
                 </label>
                 <label>
                     Write your review
-                    <textarea rows={4} cols={40}/>
+                    <textarea value={desc} onChange={handleDescChange} rows={4} cols={40}/>
                 </label>
                 <button type="reset">Reset</button>
                 <button type="submit">Add Review</button>
