94e890c9ab000e6ba4d46f1ccb59d75ff73dde29
Joshua Thomas <jthoma10@hotmail.com>
2023-12-09 20:11:29 -0500

    Updates

--- jt-part-picker/src/components/PartReviews.jsx
+++ jt-part-picker/src/components/PartReviews.jsx
@@ -1,7 +1,42 @@
+import { useNavigate, useParams, Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function PartReviews() {
-    return(
-        <div>
-            
-        </div>
-    )
+
+    let {name} = useParams()
+    const [reviews, setReviews] = useState()
+    let navigate = useNavigate()
+
+    console.log(name)
+
+    useEffect(() => {
+        const getReviews = async () => {
+            const response = await axios.get(`http://localhost:3001/review`)
+            setReviews(response.data)
+            console.log(response.data)
+        }
+        getReviews()
+    }, [])
+
+    const showReview = (key) => {
+        navigate(`${key}`)
+    }
+
+
+
+    return reviews ?(
+        <>
+            <div>
+                {reviews.filter(review => review.part == name).map((review,key) => (
+                    <div key={key} className="reviewCard" onClick={()=>showReview(review._id)}>
+                        <h2>{review.title}</h2>
+                        <h3>{review.part}</h3>
+                        <h4>{review.description}</h4>
+                    </div>
+                ))}
+            </div>
+            <Link to="/parts">Return to Parts List</Link>           
+        </>
+    ) : <h3>No Review Yet</h3>
 }
--- jt-part-picker/src/components/ReviewDetail.jsx
+++ jt-part-picker/src/components/ReviewDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
-import { useParams, Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
+
 
 
 export default function ReviewDetail() {
