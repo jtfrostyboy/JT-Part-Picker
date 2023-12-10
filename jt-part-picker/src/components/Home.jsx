da37ee3e0c98f1defbad359980992f282447f5df
Joshua Thomas <jthoma10@hotmail.com>
2023-12-10 17:28:15 -0500

    Updates

--- jt-part-picker/src/components/Home.jsx
+++ jt-part-picker/src/components/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
         <div className="home">
             <Header/>
             <div className="homeArticles">
-                <p>Computer part reviews play a crucial role in guiding consumers through the complex landscape of hardware choices. These reviews serve as valuable insights into the performance, reliability, and compatibility of various components such as processors, graphics cards, memory modules, and storage devices. Users often rely on the firsthand experiences shared by others to make informed decisions about which computer parts best suit their needs. Detailed reviews typically highlight aspects like speed, efficiency, and ease of installation, offering prospective buyers a glimpse into real-world usage scenarios. Additionally, these reviews contribute to the broader tech community by fostering discussions, sharing troubleshooting tips, and staying abreast of the latest technological advancements. As the technology landscape continues to evolve rapidly, computer part reviews remain an indispensable resource for enthusiasts, professionals, and casual users alike, ensuring that individuals can make well-informed choices when assembling or upgrading their computing systems.</p>
+                <p className="mover">Find the part you want!</p>
+                <p>Computer part reviews play a crucial role in guiding consumers through the complex landscape of hardware choices. These reviews serve as valuable insights into the performance, reliability, and compatibility of various components such as processors, graphics cards, memory modules, and storage devices. Users often rely on the firsthand experiences shared by others to make informed decisions about which computer parts best suit their needs.</p>
             </div>
         </div>
     )
